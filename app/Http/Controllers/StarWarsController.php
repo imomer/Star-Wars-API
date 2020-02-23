585703257d711a4c6f5ed480181fb2102ccf9b61
imomer <omerblink@gmail.com>
2020-02-23 12:05:51 +0500

    Feature added to get most appeared character in films.

--- app/Http/Controllers/StarWarsController.php
+++ app/Http/Controllers/StarWarsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Film;
+use App\People;
 use Illuminate\Http\Request;
 
 class StarWarsController extends Controller
@@ -21,5 +22,24 @@ class StarWarsController extends Controller
 		return response()->json( $films, 200 );
 
 	}
+
+
+	/**
+	 * Get character with highest appearances in films
+	 *
+	 * @return \Illuminate\Http\JsonResponse
+	 */
+	public function GetMostAppearedCharacter() {
+
+		// Get characters with films count
+		$characters = People::withCount( [ 'films' ] )
+		                    ->orderBy( 'films_count', 'DESC' )
+		                    ->take( 1 )
+		                    ->get();
+
+		return response()->json( $characters, 200 );
+
+	}
+
 
 }
