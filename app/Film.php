cf3f6861c4035da30d0246f0d632cd39a3c785a8
imomer <omerblink@gmail.com>
2020-02-24 01:45:49 +0500

    Feature added to get most appeared species.

--- app/Film.php
+++ app/Film.php
@@ -6,5 +6,33 @@ use Illuminate\Database\Eloquent\Model;
 
 class Film extends Model
 {
-    //
+
+	/**
+	 * Relation to characters
+	 *
+	 * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+	 */
+	public function characters()
+	{
+		return $this->belongsToMany('App\People',
+			'films_characters',
+			'film_id',
+			'people_id');
+	}
+
+
+	/**
+	 * Relation to species
+	 *
+	 * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+	 */
+	public function species()
+	{
+		return $this->belongsToMany('App\Species',
+			'films_species',
+			'film_id',
+			'species_id'
+		);
+	}
+
 }
