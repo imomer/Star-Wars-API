4b415fb48b69f3f0fdb491274a3a807f338f3242
imomer <omerblink@gmail.com>
2020-02-23 11:28:34 +0500

    Feature added to get film with longest crawl.

--- app/Http/Controllers/StarWarsController.php
+++ app/Http/Controllers/StarWarsController.php
@@ -2,9 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Film;
 use Illuminate\Http\Request;
 
 class StarWarsController extends Controller
 {
-    //
+
+	/**
+	 * Get longest opening crawl films
+	 *
+	 * @return \Illuminate\Http\JsonResponse
+	 */
+	public function GetLongestOpeningCrawlFilm() {
+
+		// Get film with longest opening crawl length
+		$films = Film::orderByRaw( 'LENGTH(opening_crawl) DESC' )->take( 1 )->get();
+
+		return response()->json( $films, 200 );
+
+	}
+
 }
