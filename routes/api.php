172993f1c887b15cb195f587bdee76777f0bef0a
imomer <omerblink@gmail.com>
2020-02-24 11:05:38 +0500

    Crrection in API route.

--- routes/api.php
+++ routes/api.php
@@ -24,4 +24,4 @@ Route::get('longest-opening-crawl-film', 'StarWarsController@GetLongestOpeningCr
 Route::get('most-appeared-character', 'StarWarsController@GetMostAppearedCharacter');
 
 // Get planets with pilots
-Route::get('get-planets-with-pilots', 'StarWarsController@GetPlanetsWithPilots');
+Route::get('planets-with-pilots', 'StarWarsController@GetPlanetsWithPilots');
